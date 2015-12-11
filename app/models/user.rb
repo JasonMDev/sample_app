c407608b8f59890a25aa832797da5546ac57065a
JasonMDev <jasonmansdev@gmail.com>
2015-12-11 14:54:32 +0000

    Solved exercise 6.5.2.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
+	#before_save { self.email = email.downcase } #- equivalent
 	validates :name, presence: true, length: { maximum: 50 }
-	#validates(:name, presence: true) - equivalent
+	#validates(:name, presence: true) #- equivalent
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
 										format: { with: VALID_EMAIL_REGEX },
