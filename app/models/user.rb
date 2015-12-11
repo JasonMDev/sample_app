934a879b886dd93bd2a0bec03e4c923811832fe9
JasonMDev <jasonmansdev@gmail.com>
2015-12-11 15:01:06 +0000

    Solved exercise 6.5.3.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
+	#----Original Method----
+	#before_save { self.email = email.downcase } 
+	#----Bang Method----
 	before_save { email.downcase! }
-	#before_save { self.email = email.downcase } #- equivalent
+	
+	#----Original----
 	validates :name, presence: true, length: { maximum: 50 }
-	#validates(:name, presence: true) #- equivalent
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#----Equivalent----
+	#validates(:name, presence: true)
+
+	#---- Doesn't check "..com" emails----
+	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#---- Does check "..com" emails----
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	
 	validates :email, presence: true, length: { maximum: 255 }, 
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: {case_sensitive: false }
