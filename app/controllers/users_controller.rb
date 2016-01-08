2514e58666cd8e5d94818a53f5187d5b2cba75c4
JasonMDev <jasonmansdev@gmail.com>
2016-01-08 09:00:44 +0000

    Tried error messages full.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+  	@user = User.find(params[:id])
+  	# | byebug debugger command.
+  	#debugger  	
+  	# | Use "debugger" and Rails console will be added in the
+  	# | rails server console. 
+  	# | When happy, Ctrl+D to resume execution.
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
+
