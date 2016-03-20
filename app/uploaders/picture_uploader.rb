2a0422d9a0bd702c45196bfef9edbc53348471bb
JasonMDev <jasonmansdev@gmail.com>
2016-03-20 23:11:10 +0000

    CHange made for picture uplodaer.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,12 +10,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :dropbox
-  else
-    storage :file
-  end
 
+  storage :file
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
